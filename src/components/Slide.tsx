d80c3fc473bc5e78c5a8f8e67c39135110be1a51
Magusin <mimi.magusin@noordhoff.nl>
2020-05-25 14:47:36 +0200

    Default slide

---
+++ src/components/Slide.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Slide.css';
+
+interface IProps {}
+
+const Slide: React.FC<IProps> = () => {
+  return <div className="Slide"><div className="Content"></div></div>
+}
+
+export default Slide;
